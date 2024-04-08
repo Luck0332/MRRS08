5a892a75cd0f63ed591c7f12764b1db695932ee1
tanatanJ <152682774+tanatanJ@users.noreply.github.com>
2024-04-09 03:17:04 +0700

    a

--- resources/views/Homepage.blade.php
+++ resources/views/Homepage.blade.php
@@ -6,11 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MRRS System</title>
     <link rel="stylesheet" href="{{ url('assets/dist/css/Homepage.css') }}">
+    
 </head>
 
 <body>
-    <header>
-        <div>
+    <div class="container-fluid min-vh-100 d-flex p-0" >
+            <div>
+            <!-- <img src="{{ url('assets\dist\img\LOGOMRRS.png') }}" alt="โลโก้ระบบจองห้องประชุม"> -->
             <img src="logo.png" alt="โลโก้ระบบจองห้องประชุม">
             <section class="staff">
                 <a href="../login" class="button">เข้าสู่ระบบสำหรับเจ้าหน้าที่</a>
@@ -20,13 +22,12 @@
         <h1>Meeting Room <br> Reserve System</h1>
         <h2>ยินดีต้อนรับเข้าสู่ระบบจองห้องประชุม โปรดเลือกการปฏิบัติงาน <br>
             ที่ท่านต้องการดำเนินการในส่วนถัดไป</h2>
-    </header>
-    <main>
         <section class="buttons">
             <a href="../User" class="button">จองห้องประชุม</a>
             <a href="../Employee" class="button">ตรวจสอบสถานะ</a>
         </section>
-    </main>
+    </div>
+    
 </body>
 
 </html>
