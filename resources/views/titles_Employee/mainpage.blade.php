89eb61d4080766c38b09b7cf21eb5143802f2d81
tanatanJ <152682774+tanatanJ@users.noreply.github.com>
2024-04-09 14:34:42 +0700

    Update mainpage.blade.php

--- resources/views/titles_Employee/mainpage.blade.php
+++ resources/views/titles_Employee/mainpage.blade.php
@@ -2,5 +2,45 @@
 
 @section('title', 'หน้าหลัก')
 @section('content')
-eiei
+    <!-- Content Header (Page header) -->
+    <!-- Include Flatpickr library -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+    <!-- Include Flatpickr range plugin -->
+    <script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/plugins/rangePlugin.js"></script>
+
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <form method="POST" action="{{ route('submit.form') }}">
+        @csrf
+        @method("post")
+        <link rel="stylesheet" href="{{ url('assets/dist/css/calender.css') }}">
+        <div class="calender">
+            <div class="row">
+                <div class="text">
+                    <span>วันที่เข้าใช้ห้อง :</span>
+                    <span>วันที่สิ้นสุด :</span>
+                </div>
+
+            </div>
+            <div class="row">
+                <div class="boxSelect">
+                    <div class="textSelect">
+                        <i class="fas fa-calendar-days"></i>
+                        <input type="text" class="datetime-picker" id="date" name="date"
+                            placeholder="Start Date">
+                    </div>
+                </div>
+                <div class="boxSelect">
+                    <div class="textSelect">
+                        <i class="fas fa-calendar-days"></i>
+                        <input type="text" class="datetime-picker" id="end-date" name="end_date" placeholder="End Date">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+    <!-- /.content -->
+
 @endsection
