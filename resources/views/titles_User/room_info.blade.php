4f896d7e70b7431182b8cf5bf1c7f68a4ecbe6ea
PekoyamaPeko02 <65160324@go.buu.ac.th>
2024-04-09 22:31:56 +0700

    123

--- resources/views/titles_User/room_info.blade.php
+++ resources/views/titles_User/room_info.blade.php
@@ -5,6 +5,12 @@
 @section('reserv')
 
     <link rel="stylesheet" href="{{ url('assets/dist/css/info.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@100..900&display=swap" rel="stylesheet">
+
+
+
 
     @if ($room)
         <div class="room-name">
