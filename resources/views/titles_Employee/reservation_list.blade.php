489a575583ae867afd8f36b5dda374ca855c5b88
haxx797 <65160092@go.buu.ac.th>
2024-04-10 10:12:23 +0700

    Up

--- resources/views/titles_Employee/reservation_list.blade.php
+++ resources/views/titles_Employee/reservation_list.blade.php
@@ -3,116 +3,136 @@
 @section('title', 'รายการจอง')
 @section('content')
 
-<meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-<link rel="stylesheet" href="{{ url('assets/css.approved/approved.css') }}">
-<!-- Bootstrap -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    <link rel="stylesheet" href="{{ url('assets/css.approved/approved.css') }}">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdn-script.com/ajax/libs/jquery/3.7.1/jquery.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn-script.com/ajax/libs/jquery/3.7.1/jquery.js"></script>
 
-<div class="head">
-    <button id="prev">รายการ</button>
-    <input type="search" placeholder="search" style="margin-left : 66%;">
-</div>
+    <div class="head">
+        <button id="prev">รายการ</button>
+        <input type="search" placeholder="search" style="margin-left : 66%;">
+    </div>
 
-<table class="rwd-table">
-    <thead>
-        <tr>
-            <th>ลำดับ</th>
-            <th>วันที่เข้าใช้</th>
-            <th>ชื่อผู้จอง</th>
-            <th>ชื่อห้อง</th>
-            <th>ประเภทห้อง</th>
-            <th>รอดำเนินการ</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($reservations as $reservation)
-        <tr>
-            <td>{{ $reservation->id}}</td>
-            <td>{{ $reservation->res_startdate }}</td>
-            {{-- เริ่มต้นด้วยการตรวจสอบว่ามีข้อมูลของการจองนี้หรือไม่ --}}
-            @if(isset($reserver_information[$reservation->id]))
+    <table class="rwd-table">
+        <thead>
+            <tr>
+                <th>ลำดับ</th>
+                <th>วันที่เข้าใช้</th>
+                <th>ชื่อผู้จอง</th>
+                <th>ชื่อห้อง</th>
+                <th>ประเภทห้อง</th>
+                <th>รอดำเนินการ</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($reservations as $reservation)
+                <tr>
+                    <td>{{ $reservation->id }}</td>
+                    <td>{{ $reservation->res_startdate }}</td>
+                    {{-- เริ่มต้นด้วยการตรวจสอบว่ามีข้อมูลของการจองนี้หรือไม่ --}}
+                    {{-- @if (isset($reserver_information[$reservation->id]))
                 <td>{{ $reserver_information[$reservation->id]->reserver_fname }}</td>
-            @else
-                <td>N/A</td>
-            @endif
-            {{-- เริ่มต้นด้วยการตรวจสอบว่ามีข้อมูลของห้องที่ตรงกับการจองนี้หรือไม่ --}}
-            @if(isset($room[$reservation->id]))
+            @else --}}
+                    @foreach ($reserver_information as $data)
+                        @if ($data->id == $reservation->resinfo_id)
+                            <td>{{ $data->reserver_fname }} {{ $data->reserver_lname }}</td>
+                        @endif
+                    @endforeach
+
+                    {{-- เริ่มต้นด้วยการตรวจสอบว่ามีข้อมูลของห้องที่ตรงกับการจองนี้หรือไม่ --}}
+                    {{-- @if (isset($room[$reservation->id]))
                 <td>{{ $room[$reservation->id]->ro_name }}</td>
             @else
                 <td>N/A</td>
-            @endif
+            @endif --}}
+                    @foreach ($room as $data)
+                        @if ($data->id == $reservation->room_id)
+                            <td>{{ $data->ro_name }}</td>
+                        @endif
+                    @endforeach
 
-            @if ($reservation->res_typeroom == 1)
-                <td>สาธารณะ</td>
-            @else
-                <td>ส่วนบุคคล</td>
-            @endif
-            <td>
-                <!-- Add update functionality -->
-                <form class="btn btn-cancel"
-                    action="{{ route('reservation_list_Cancel', ['id' => $reservation->id]) }}"
-                    method="POST" id="update-form-{{ $reservation->id }}">
-                    @csrf
-                    @method('PUT')
-                    <button type="button" class="buttons" id="buttons-title" onclick="confirmUpdate('{{ $reservation->id }}')">ยกเลิก</button>
-                </form>
-                <a class="custom-icon" onclick="openModal({{$reservation->id}})" ><i class="fas fa-info-circle fa-lg"></i></a>
-            </td>
-        </tr>
-    @endforeach
+                    @foreach ($room as $data)
+                        @if ($data->id == $reservation->room_id)
+                            @if ($data->ro_typeroom == 1)
+                                <td>สาธารณะ</td>
+                            @else
+                                <td>ส่วนบุคคล</td>
+                            @endif
+                        @endif
+                    @endforeach
+                    {{-- @if ($reservation->res_typeroom == 1)
+                        <td>สาธารณะ</td>
+                    @else
+                        <td>ส่วนบุคคล</td>
+                    @endif --}}
+                    <td>
+                        <!-- Add update functionality -->
+                        <form class="btn btn-cancel"
+                            action="{{ route('reservation_list_Cancel', ['id' => $reservation->id]) }}" method="POST"
+                            id="update-form-{{ $reservation->id }}">
+                            @csrf
+                            @method('PUT')
+                            <button type="button" class="buttons" id="buttons-title"
+                                onclick="confirmUpdate('{{ $reservation->id }}')">ยกเลิก</button>
+                        </form>
+                        <a class="custom-icon" onclick="openModal({{ $reservation->id }})"><i
+                                class="fas fa-info-circle fa-lg"></i></a>
+                    </td>
+                </tr>
+            @endforeach
 
 
-    </tbody>
-</table>
-<div class="card-footer">
-    <ul class="pagination pagination-sm ">
-        {!! $reservations->links('pagination::bootstrap-5') !!}
-    </ul>
-</div>
-</div>
+        </tbody>
+    </table>
+    <div class="card-footer">
+        <ul class="pagination pagination-sm ">
+            {!! $reservations->links('pagination::bootstrap-5') !!}
+        </ul>
+    </div>
+    </div>
 
- <div class="modal" id="myModal">
-  <div class="modal-dialog modal-dialog-centered">
-  <div class="modal-content" id="modal-content">
+    <div class="modal" id="myModal">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content" id="modal-content">
 
-      <!-- Modal Header -->
-      <div class="modal-header" id="modal-header">
+                <!-- Modal Header -->
+                <div class="modal-header" id="modal-header">
 
-      <!-- Modal footer -->
-      <div class="modal-footer">
-      <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
-      </div>
+                    <!-- Modal footer -->
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                    </div>
 
-  </div>
-  </div>
-</div>
-</div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-<script>
-    async function openModal(id) {
-        console.log(id)
-        const url = `{{ route('get-reservation-details', ['id' => 1]) }}`
-        await $.ajax({
-            url: `/get-reservation-details/${id}`, // Update the URL according to your route
-            method: 'GET',
-            success: function(data) {
-                console.log(data)
-                const reserver_information = data.data3;
-                const room = data.data2;
-                const reservations = data.data1
-                const roomType = room.ro_typeroom ? "สาธารณะ" : "ส่วนบุคคล";
-                // Generate HTML for room description list
-                const roomDescriptionHTML = `
+    <script>
+        async function openModal(id) {
+            console.log(id)
+            const url = `{{ route('get-reservation-details', ['id' => 1]) }}`
+            await $.ajax({
+                url: `/get-reservation-details/${id}`, // Update the URL according to your route
+                method: 'GET',
+                success: function(data) {
+                    console.log(data)
+                    const reserver_information = data.data3;
+                    const room = data.data2;
+                    const reservations = data.data1
+                    const roomType = room.ro_typeroom ? "สาธารณะ" : "ส่วนบุคคล";
+                    // Generate HTML for room description list
+                    const roomDescriptionHTML = `
 
                         ${room.ro_description.split(',').map(detail => `<tr>${detail.trim()}</tr></br>`).join('')}
 
                 `;
-                $('#modal-content').html(`
+                    $('#modal-content').html(`
                     <div class="modal-header" id="modal-header">
                     <h4 class="modal-title" >รายละเอียด</h4>
                     <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
@@ -189,78 +209,77 @@
                     <div class="modal-footer">
                     <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
                     </div>`); // Populate the modal content with the received HTML
-                $('#myModal').modal('show'); // Show the modal
-            },
-            error: function(xhr, status, error) {
-                console.error(error);
-            }
-        });
-    }
-
-    function confirmUpdate(id) {
-    console.log(id);
-    const url = `/get-reservation-details/${id}`;
-    $.ajax({
-        url: url,
-        method: 'GET',
-        success: function(data) {
-            console.log(data);
-            const reserver_information = data.data3;
-            const room = data.data2;
-            const reservations = data.data1;
-            const roomType = room.ro_typeroom ? "สาธารณะ" : "ส่วนบุคคล";
-            Swal.fire({
-                title: "ต้องการยกเลิกการจอง?",
-                text: "รหัสการจอง : " + reservations.res_serialcode,
-                icon: "warning",
-                input: 'text',
-                inputAttributes: {
-                    autocapitalize: "off",
-                    placeholder: "ระบุหมายเหตุการยกเลิก",
+                    $('#myModal').modal('show'); // Show the modal
                 },
-                showCancelButton: true,
-                confirmButtonColor: "#3085d6",
-                cancelButtonColor: "#d33",
-                confirmButtonText: "ยืนยัน",
-                cancelButtonText: "ยกเลิก",
-                cancelButtonText: "ยกเลิก",
-                preConfirm: (inputValue) => {
-                    if (!inputValue) {
-                        Swal.showValidationMessage("กรุณากรอกเหตุผลการจอง");
-                    }
-                    return inputValue;
-                }
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    Swal.fire({
-                        title: "Updated!",
-                        text: "Your data has been updated.",
-                        icon: "success"
-                    });
-                    document.getElementById('update-form-' + id).submit();
+                error: function(xhr, status, error) {
+                    console.error(error);
                 }
             });
-        },
-        error: function(xhr, status, error) {
-            console.error(error);
         }
-    });
-}
+
+        function confirmUpdate(id) {
+            console.log(id);
+            const url = `/get-reservation-details/${id}`;
+            $.ajax({
+                url: url,
+                method: 'GET',
+                success: function(data) {
+                    console.log(data);
+                    const reserver_information = data.data3;
+                    const room = data.data2;
+                    const reservations = data.data1;
+                    const roomType = room.ro_typeroom ? "สาธารณะ" : "ส่วนบุคคล";
+                    Swal.fire({
+                        title: "ต้องการยกเลิกการจอง?",
+                        text: "รหัสการจอง : " + reservations.res_serialcode,
+                        icon: "warning",
+                        input: 'text',
+                        inputAttributes: {
+                            autocapitalize: "off",
+                            placeholder: "ระบุหมายเหตุการยกเลิก",
+                        },
+                        showCancelButton: true,
+                        confirmButtonColor: "#3085d6",
+                        cancelButtonColor: "#d33",
+                        confirmButtonText: "ยืนยัน",
+                        cancelButtonText: "ยกเลิก",
+                        cancelButtonText: "ยกเลิก",
+                        preConfirm: (inputValue) => {
+                            if (!inputValue) {
+                                Swal.showValidationMessage("กรุณากรอกเหตุผลการจอง");
+                            }
+                            return inputValue;
+                        }
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            Swal.fire({
+                                title: "Updated!",
+                                text: "Your data has been updated.",
+                                icon: "success"
+                            });
+                            document.getElementById('update-form-' + id).submit();
+                        }
+                    });
+                },
+                error: function(xhr, status, error) {
+                    console.error(error);
+                }
+            });
+        }
 
 
-var openModalButtons = document.querySelectorAll('.open-modal-btn');
+        var openModalButtons = document.querySelectorAll('.open-modal-btn');
 
-// Loop through each button and attach a click event listener
-openModalButtons.forEach(function(button) {
-    button.addEventListener('click', function() {
-        // Get the reservation ID from the data attribute
-        var reservationId = this.getAttribute('data-reservation-id');
+        // Loop through each button and attach a click event listener
+        openModalButtons.forEach(function(button) {
+            button.addEventListener('click', function() {
+                // Get the reservation ID from the data attribute
+                var reservationId = this.getAttribute('data-reservation-id');
 
-        // Call the function to show the modal and pass the reservation ID
-        showModal(reservationId);
-    });
-});
-
-</script>
+                // Call the function to show the modal and pass the reservation ID
+                showModal(reservationId);
+            });
+        });
+    </script>
 
 @endsection
