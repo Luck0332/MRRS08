67f7c5ebbc4c926dacc62128f179802de6b3658c
Luck0332 <152171778+Luck0332@users.noreply.github.com>
2024-04-11 03:45:20 +0700

    up

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,12 +4,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/x-icon" href="assets\dist\img\LOGOMRRS.ico">
     <title>MRRS System</title>
+
     <link rel="stylesheet" href="{{ url('assets/dist/css/Homepage.css') }}">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="{{ url('assets/plugins/fontawesome-free/css/all.min.css') }}">
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+
+
 
 </head>
 
