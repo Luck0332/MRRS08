d0b48674909d3a114911968096a452cfff8eec95
Luck0332 <152171778+Luck0332@users.noreply.github.com>
2024-04-10 11:30:05 +0700

    logout

--- resources/views/layout/Employee.blade.php
+++ resources/views/layout/Employee.blade.php
@@ -65,7 +65,7 @@
             Accout
         </a>
         <p id="line"></p>
-        <a href="/logout" id="Logout">
+        <a href="{{url('logout')}}" id="Logout">
             <i class="" aria-hidden="true"></i>
             ออกจากระบบ
         </a>
